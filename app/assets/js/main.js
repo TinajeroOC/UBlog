6bf32e0929867d8cb9c3094e8f296ea283f591a5
TinajeroOC <88462409+TinajeroOC@users.noreply.github.com>
2023-10-01 12:37:13 -0700

    Improve alert detection on button click.

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,7 +1,1 @@
-// YOUR CODE GOES HERE:
-document.addEventListener("click", event => {
-  const target = event.target.parentElement.parentElement
-  if (target.classList.contains("alert")) {
-    target.remove()
-  }
-});
+document.addEventListener("click", event => event.target.closest(".alert")?.remove());
