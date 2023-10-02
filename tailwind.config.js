36272e63b3006fb836629eb79bccb230e2946194
TinajeroOC <88462409+TinajeroOC@users.noreply.github.com>
2023-10-02 09:05:59 -0700

    Use line clamps and position buttons to bottom on blog cards.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./app/**/*.{hbs,js}"],
+  theme: {
+    extend: {
+      lineClamp: {
+        8: '8',
+        10: '10',
+        12: '12',
+        14: '14',
+        16: '16',
+      }
+    }
+  },
   plugins: [require("daisyui")],
   daisyui: {
     themes: ["light", "dark"],
